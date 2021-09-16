931f5a2e37070a8df963522d15e58253340719f2
AldiAgeng <aldiageng48@gmail.com>
2021-09-16 10:46:02 +0700

    Selesai latihan video Constructor

---
+++ constructor.php
@@ -0,0 +1,43 @@
+<!-- VIDEO TENTANG Constructor -->
+<?php 
+// Membuat Class Produk
+  class Produk{
+    // Property
+    public $judul = "judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0;
+
+    // Konstruktor
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+    {
+      $this->judul = $judul;
+      $this->penulis = $penulis;
+      $this->penerbit = $penerbit;
+      $this->harga = $harga;
+    }
+
+    // Method getLabel
+    public function getLabel(){
+      return "$this->penulis, $this->penerbit";
+    }
+  }
+  // Instansiasi
+  // $produk1 = new Produk();
+  // $produk1->judul = "Naruto";
+  // var_dump($produk1);
+
+  // $produk2 = new Produk();
+  // $produk2->judul = "Uncharted";
+  // $produk2->tambahProperty = "hahaha";
+  // var_dump($produk2);
+
+
+  $produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump",30000);
+  $produk2 = new Produk("Uncharted","Neil Druckmann","Sony Computer",250000);
+  $produk3 =  new Produk("Dragon Ball");
+
+//Menampilkan ke layar 
+  echo "Komik : ".$produk1->getLabel();
+  echo "<br>";
+  echo "Game : ".$produk2->getLabel();
+  echo "<br>";
+  var_dump($produk3);
+?>
