eec1e9aa0de6f86b6e5521020e0a9392ccb068cf
AldiAgeng <aldiageng48@gmail.com>
2021-09-16 08:38:52 +0700

    menyelesaikan latihan di video kelas dan objek

---
+++ coba.php
@@ -0,0 +1,11 @@
+<!-- VIDEO KEDUA -->
+<?php 
+// Membuat Class Coba
+class Coba{
+
+}
+// Melakukan Instansiasi class Coba
+$a = new Coba();
+// Mencetak output menggunakan var_dump
+var_dump($a);
+?>
