5351ff95aadc9377080450e9da109fd69bf7bbca
AldiAgeng <aldiageng48@gmail.com>
2021-09-23 09:44:12 +0700

    Selesai latihan video Overriding

---
+++ overriding.php
@@ -0,0 +1,80 @@
+<!-- VIDEO TENTANG Overriding -->
+<?php 
+// Membuat Class Produk
+class Produk{
+    // Property
+    public $judul, $penulis, $penerbit, $harga;
+
+    // Konstruktor
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+    {
+      $this->judul = $judul;
+      $this->penulis = $penulis;
+      $this->penerbit = $penerbit;
+      $this->harga = $harga;
+    }
+
+    // Method getLabel
+    public function getLabel(){
+      return "$this->penulis, $this->penerbit";
+    }
+
+    // Method getInfoLengkap
+    public function getInfoProduk(){
+      $str = "{$this->judul} | {$this->getLabel()} | (Rp.{$this->harga})";
+      return $str;
+    }
+}
+
+// kelas Komik
+class Komik extends Produk {
+  public $jmlHalaman;
+  
+  public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0,
+  $jmlHalaman = 0){
+    parent::__construct(  $judul, $penulis, $penerbit, $harga );
+    $this->jmlHalaman = $jmlHalaman;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Komik : ". parent::getInfoProduk() ." - {$this->jmlHalaman} Halaman.";
+    return $str;
+  }
+}
+
+// kelas Game
+class Game extends Produk {
+  public $waktuMain;
+
+  public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0,
+  $waktuMain = 0)
+  {
+    parent::__construct( $judul, $penulis, $penerbit, $harga);
+    $this->waktuMain = $waktuMain;
+  }
+  public function getInfoProduk()
+  {
+    $str = "Game : ". parent::getInfoProduk() ." ~ {$this->waktuMain} Jam.";
+    return $str;
+  }
+}
+
+// Kelas Cetak Info produk
+class CetakInfoProduk{
+  // Method cetak
+  public function cetak(Produk $produk){
+    $str = "{$produk->judul} | {$produk->getLabel()} Rp.{$produk->harga}";
+    return $str;
+  }
+}
+
+  // Instansiasi
+  $produk1 = new Komik("Naruto","Masashi Kishimoto","Shonen Jump",30000, 100);
+  $produk2 = new Game("Uncharted","Neil Druckmann","Sony Computer",250000, 50);
+
+//Menampilkan ke layar 
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+?>
